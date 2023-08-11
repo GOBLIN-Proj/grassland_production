4a2d26ac8e5c7a57f49df8837859afc2eeb7562a
Colm <colm.p.duffy@gmail.com>
2023-08-11 13:33:48 +0100

    bug fix issue #16

--- src/grassland_production/grassland_area.py
+++ src/grassland_production/grassland_area.py
@@ -10,17 +10,30 @@ class Areas:
         self.calibration_year = calibration_year
         self.default_calibration_year = default_calibration_year
 
+
     def get_proportion_weight(
-        self, area_nfs, farm_system_number, calibration_year, system
+        self, area_nfs, farm_system_number, nfs_dict, calibration_year, system, grassland_type
     ):
-        return area_nfs * (
-            farm_system_number.loc[calibration_year, system]
-            / (
-                (area_nfs * farm_system_number.loc[calibration_year, "dairy"])
-                + (area_nfs * farm_system_number.loc[calibration_year, "beef"])
-                + (area_nfs * farm_system_number.loc[calibration_year, "sheep"])
+        
+        total = (
+                (nfs_dict["dairy"].loc[calibration_year,grassland_type].item() * farm_system_number.loc[calibration_year, "dairy"].item())
+                + (nfs_dict["beef"].loc[calibration_year,grassland_type].item() * farm_system_number.loc[calibration_year, "beef"].item())
+                + (nfs_dict["sheep"].loc[calibration_year,grassland_type].item()* farm_system_number.loc[calibration_year, "sheep"].item())
             )
-        )
+        
+        system_area = area_nfs * farm_system_number.loc[calibration_year, system].item()
+        
+        result = system_area/total
+
+        return result
+        
+
+    def get_total_nfs_areas_for_proportions(self, dairy_area_nfs, beef_area_nfs, sheep_area_nfs):
+
+        combined_dataframe = dairy_area_nfs + beef_area_nfs + sheep_area_nfs
+
+        return combined_dataframe
+    
 
     def get_nfs_system_proportions(self):
         grassland_types = ["Grass silage", "Hay", "Pasture", "Rough grazing in use"]
@@ -28,6 +41,14 @@ class Areas:
         dairy_area_nfs = self.loader_class.dairy_area_nfs()
         beef_area_nfs = self.loader_class.beef_area_nfs()
         sheep_area_nfs = self.loader_class.sheep_area_nfs()
+
+        nfs_dict= {
+            "dairy": dairy_area_nfs,
+            "beef": dairy_area_nfs,
+            "sheep": dairy_area_nfs,
+        }
+
+        #total_areas_nfs = self.get_total_nfs_areas_for_proportions(dairy_area_nfs, beef_area_nfs, sheep_area_nfs)
 
         farm_system_number = self.loader_class.nfs_farm_numbers()
 
@@ -41,6 +62,8 @@ class Areas:
             0, columns=dairy_area_nfs.columns, index=dairy_area_nfs.index
         )
 
+
+
         systems_dict = {
             "dairy": dairy_nfs_system_proportions,
             "beef": beef_nfs_system_proportions,
@@ -48,12 +71,13 @@ class Areas:
         }
 
         default_year_flag = False
-        for ix, grassland_type, sys in product(
-            dairy_nfs_system_proportions.index, grassland_types, systems_dict.keys()
+        for sys, grassland_type, ix in product(
+            systems_dict.keys(), grassland_types, dairy_nfs_system_proportions.index
         ):
+            
             try:
-                systems_dict[sys].loc[ix, grassland_type] = self.get_proportion_weight(
-                    dairy_area_nfs.loc[ix, grassland_type], farm_system_number, ix, sys
+                systems_dict[sys].at[ix, grassland_type] = self.get_proportion_weight(
+                    nfs_dict[sys].loc[ix, grassland_type], farm_system_number, nfs_dict, ix, sys, grassland_type
                 )
             except KeyError:
                 if default_year_flag == True:
@@ -62,14 +86,17 @@ class Areas:
                     )
                     default_year_flag = False
 
-                systems_dict[sys].loc[ix, grassland_type] = self.get_proportion_weight(
-                    dairy_area_nfs.loc[ix, grassland_type],
+                systems_dict[sys].at[ix, grassland_type] = self.get_proportion_weight(
+                    nfs_dict[sys].loc[self.default_calibration_year, grassland_type],
                     farm_system_number,
+                    nfs_dict,
                     self.default_calibration_year,
                     sys,
+                    grassland_type
                 )
 
         return systems_dict["dairy"], systems_dict["beef"], systems_dict["sheep"]
+    
 
     def get_nfs_within_system_grassland_distribution(self):
         dairy_area_nfs = self.loader_class.dairy_area_nfs()
